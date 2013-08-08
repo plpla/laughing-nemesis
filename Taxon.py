69a46ddf11824cef1689f6df484940a4c4f5c443
Pier-Luc Plante <pier-luc.plante.1@ulaval.ca>
2013-08-08 10:22:22 -0400

    Finished the base of the tree and added some test code. Signed-off-by: Pier-Luc Plante <pier-luc.plante.1@ulaval.ca>

--- Taxon.py
+++ Taxon.py
@@ -9,11 +9,11 @@ import threading
 import logging	#for debuging
 import Error
 import FileUtility
-
+import sys
 
 class Taxon():
 	def  __init__(self, taxonid):
-		self.ID==taxonid;
+		self.ID=taxonid;
 		self.Parent="";
 		self.Kids=[];
 	
@@ -23,17 +23,18 @@ class Taxon():
 			a=1;
 		return a;
 
-	def hasKid(sef, taxonId):
+	def hasKid(self, taxonId):
 		a=0;
-		if(taxonId in self.Kids):
-			a=1;
+		if(not self.Kids):
+			if(taxonId in self.Kids):
+				a=1;
 		return a;
 
 	def getKids(self):
 		return self.Kids;
 
 	def getParents(self):
-		return self.parents;
+		return self.Parent;
 
 	def getId(self):
 		return self.ID;
@@ -52,43 +53,70 @@ The taxonomic tree
 """
 class TaxonomicTree():
 	def __init__(self):
-		self.nodes={};
+		self.Nodes={};
+	
+	def getNodes(self):
+		return(self.Nodes);
+
 	def nodeExist(self, nodeId):
 		detect=0;
 		try:
-			self.nodes.get(nodeId);
+			self.Nodes[nodeId];
 			detect=1;
 		except:
 			detect=0;
 		return(detect);
+
 	def addNode(self, node):
 		if(self.nodeExist(node.getId())):
 			Error.error("Logic error: node already exist. Node number:"+str(node.getId()));
-		self[node.getId()]=node;
+		self.Nodes[node.getId()]=node;
+
 	def getNode(self, nodeId):
 		if(self.nodeExist(nodeId)):
-			return(self.nodes[nodeId]);
+			return self.Nodes[nodeId];
 		else:
 			Error.error("Logic error: required node does not exist");
 
 	def checkTree(self):
-		for i in self.nodes:
-			for j in self.nodes[i].getParents():
-				if(self.nodeExist(j.getId())):
-					continue;
+		numOfNode=len(self.getNodes());
+		sys.stderr.write(str(numOfNode)+ " to be checked\n");
+		checked=0;
+		root=[];
+		for i in self.Nodes:
+			#print(self.Nodes[i].getId());
+			if(self.Nodes[i].getId()==1):
+				print("my parents")
+				print(self.Nodes[i].getParents());
+				print("my kids")
+				print(self.Nodes[i].getKids());
+			if(self.Nodes[i].getParents()):
+				if(self.nodeExist(self.Nodes[i].getParents())):
+					pass;
+					#print("One parent checked")					
 				else:
-					Error.error("Logic error: tree is not valid");
-			for k in self.nodes[i].getKids():
-				if(self.nodeExist(k.getId())):
-                                        continue;
+					Error.error("Logic error: tree is not valid because this parent does not exist:"+str(self.Nodes[i].getParents()));
+			else:
+				sys.stderr.write("Found a node without parents\n")
+				root.append(self.Nodes[i].getParents());
+			for k in self.Nodes[i].getKids():
+				if(self.nodeExist(k)):
+					pass;
+					#print("One kid checked");
                                 else:
-                                        Error.error("Logic error: tree is not valid");
+                                        Error.error("Logic error: tree is not valid because this kid does not exist:"+str(k));
+			checked+=1;
+			if(checked%1000==0):
+				sys.stderr.write(str(checked)+" nodes checked\n");
+		for b in root:
+			sys.stderr.write(str(b)+"\n");
+		sys.stderr.write("Taxonomic tree is considered valid\n");
 
 
-@staticmethod
 def readTreeOfLife(file):
 	FileUtility.isValid(file);
 	tree=TaxonomicTree();
+	numOfLine=0;
 	for line in open(file):
 		parent=int(line.split()[0]);
                 kid=int(line.split()[1]);
@@ -102,7 +130,7 @@ def readTreeOfLife(file):
                                 tree.getNode(parent).addKid(kid);
                                 tree.getNode(kid).setParent(parent);
                 else:   
-                        if(tree.exist(kid)):
+                        if(tree.nodeExist(kid)):
                                 newNode=Taxon(parent);
                                 tree.addNode(parent);
                                 tree.getNode(kid).setParent(parent);
@@ -114,57 +142,17 @@ def readTreeOfLife(file):
 				newParent.addKid(kid);
 				tree.addNode(newKid);
 				tree.addNode(newParent);
+		numOfLine+=1;
+		if(numOfLine%100000==0):
+			sys.stderr.write(str(numOfLine)+" lines read\n");
+	return(tree);
 
-				
+#test main	
+if __name__=="__main__":
+	files=sys.argv[1];
+	sys.stderr.write("Loading taxonomic tree\n")
+	tree=readTreeOfLife(files);
+	sys.stderr.write("Taxonomic tree is loaded\n");
+	sys.stderr.write("Checking if tree is valid\n")
+	tree.checkTree();
 			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	
-		
-		
-
-
-
-
-
