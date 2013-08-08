93696323a74c89bde564284967543bd42ea5f63b
Pier-Luc Plante <pier-luc.plante.1@ulaval.ca>
2013-08-08 11:20:46 -0400

    Added taxon names to tree. Signed-off-by: Pier-Luc Plante <pier-luc.plante.1@ulaval.ca>

--- Taxon.py
+++ Taxon.py
@@ -11,11 +11,33 @@ import Error
 import FileUtility
 import sys
 
+class TaxonName():
+	def __init__(self):
+		self.Name="";
+		self.Rank="";
+	
+	def getName(self):
+		return(self.Name);
+
+	def getRank(self):
+		return(self.Rank);
+
+	def setAll(self, name, rank):
+		self.Name=name;
+		self.Rank=rank;
+
+	def setName(self, name):
+		self.Name=name;
+
+	def setRank(self, rank):
+		self.Rank=rank;
+
 class Taxon():
 	def  __init__(self, taxonid):
 		self.ID=taxonid;
 		self.Parent="";
 		self.Kids=[];
+		self.TaxonName=TaxonName();
 	
 	def hasParent(self, taxonId):
 		a=0;
@@ -32,6 +54,9 @@ class Taxon():
 
 	def getKids(self):
 		return self.Kids;
+
+	def getTaxonName(self):
+		return self.TaxonName;
 
 	def getParents(self):
 		return self.Parent;
@@ -77,19 +102,58 @@ class TaxonomicTree():
 			return self.Nodes[nodeId];
 		else:
 			Error.error("Logic error: required node does not exist");
-
+	#return the last common ancerstor in the tree. If it is not found, return -1 and print a warning
+	def findLCA(self, node1, node2):
+		if(self.nodeExist(node1) and self.nodeExist(node2)):
+			pass
+		else:
+			Error.error("Logic error, asked to find LCA for a node that does not exists");
+		parentsOfNode1=[];
+		parentsOfNode1.append(node1);
+		parentsOfNode2=[];
+		parentsOfNode2.append(node2);
+		lca=-1;
+		while(self.getNodes[node1].getParents()):
+			parentToAdd=self.getNodes[node1].getParents();
+			parentsOfNode1.append(parentToAdd);
+			node1=parentToAdd;
+		while(self.getNodes[node2].getParents()):
+			parentToAdd=self.getNodes[node2].getParents();
+			parentsOfNode2.append(parentToAdd);
+			node2=parentToAdd;
+		for i in parentsOfNode1:
+			if i in parentsOfNode2:
+				lca=i;
+				break;
+		if(lca==-1):
+			Errror.warning("Have not found the LCA for nodes:" +str(node1)+" and "+str(node2));
+		return i
+				
+	def addTaxonName(self, file):
+		FileUtility.isValid(file);
+		for line in open(file):
+			id=int(line.split('\t')[0]);
+			name=line.split('\t')[1];
+			rank=line.split('\t')[2];
+			if(self.nodeExist(id)):
+				self.getNodes()[id].getTaxonName().setAll(name, rank);
+			else:
+				Error.warning("A taxon is missing");
+			
+						
+		
 	def checkTree(self):
 		numOfNode=len(self.getNodes());
 		sys.stderr.write(str(numOfNode)+ " to be checked\n");
 		checked=0;
 		root=[];
 		for i in self.Nodes:
-			#print(self.Nodes[i].getId());
-			if(self.Nodes[i].getId()==1):
-				print("my parents")
-				print(self.Nodes[i].getParents());
-				print("my kids")
-				print(self.Nodes[i].getKids());
+			#print(self.Nodes[i].getId()==1);
+			#if not(self.Nodes[i].getId()):
+			#	print("my parents")
+			#	print(self.Nodes[i].getParents());
+			#	print("my kids")
+			#	print(self.Nodes[i].getKids());
 			if(self.Nodes[i].getParents()):
 				if(self.nodeExist(self.Nodes[i].getParents())):
 					pass;
@@ -106,7 +170,7 @@ class TaxonomicTree():
                                 else:
                                         Error.error("Logic error: tree is not valid because this kid does not exist:"+str(k));
 			checked+=1;
-			if(checked%1000==0):
+			if(checked%50000==0):
 				sys.stderr.write(str(checked)+" nodes checked\n");
 		for b in root:
 			sys.stderr.write(str(b)+"\n");
@@ -155,4 +219,7 @@ if __name__=="__main__":
 	sys.stderr.write("Taxonomic tree is loaded\n");
 	sys.stderr.write("Checking if tree is valid\n")
 	tree.checkTree();
+	sys.stderr.write("Adding taxon name to tree\n");
+	tree.addTaxonName(sys.argv[2]);
+	sys.stderr.write("Taxon name added to tree\n");
 			
